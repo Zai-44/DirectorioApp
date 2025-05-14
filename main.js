2e299e90441dfc4e4d9a9095958280f0ea1917fc
Zai-44 <zaira00.sand@gmail.com>
2025-05-14 11:32:06 -0600

    Ajustes

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log ("Hola");
+//En JS tenemos las funciones tipo flecha
+// utilizaremos la fetch que es un método para consumir una API
+
+//Paso unoi: Función flecha
+//metodo then o dem viene con fetch(es un metodo o una función)
+const consumirApi = () => {
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    //Los daros van a la consola
+    .then((data) => console.log(data))
+    //Descubrir que hacer en caso de error
+    .catch((error) => console.log(error));
+};
+
+consumirApi();
+
+//EN la siguinete clase agregaremos la interfaz grafica
+//Consumir otra API y mostrar  la data en la consola como la de arriba tpdo en ingles
