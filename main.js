34c143b8d1134690109323d770a1c320e316e3a6
Zai-44 <zaira00.sand@gmail.com>
2025-05-26 11:25:19 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 const consumirApi = () => {
   fetch("https://jsonplaceholder.typicode.com/users")
+    //convertimos las respuesta a JSON
     .then((response) => response.json())
+    //Los datos lo vamos a mandar la consola
     .then((data) => {
+      //puede ser otro nombre
+      //La data de la API la llevamos a HTML
+      //Definimos las constantes
       const contenedor = document.getElementById("contenedor");
-
+      //data es un array/arreglo y se cuenta desde [0]
       data.forEach((personita) => {
         const avatarUrl = `https://ui-avatars.com/api/?name=${encodeURIComponent(
           personita.name
         )}&background=random&color=fff`;
         contenedor.innerHTML += `
           <div class="col">
-            <div class="user-card h-100 p-3 shadow rounded text-white bg-dark">
+            <div class="personita-card h-100 p-3 shadow rounded text-white bg-dark">
             <div class="text-center mb-3">
                 <img src="${avatarUrl}" alt="Avatar de ${personita.name}" class="rounded-circle avatar-img" />
               </div> 
@@ -32,10 +37,12 @@ const consumirApi = () => {
 
       console.log("Usuarios cargados correctamente.");
     })
+    //Descubri que hacer en caso de que no me responda
     .catch((error) => console.log("Error al consumir la API:", error));
 };
 
 // Esperar a que el DOM esté listo
+//DOM=Document Object MOdel: Documento modelo Objto
 document.addEventListener("DOMContentLoaded", () => {
   consumirApi();
 });
